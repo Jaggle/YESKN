538f61be247318a5e533c76f90b119f0eb2827a3
Jaggle <singviy@gmail.com>
2018-08-14 23:42:15 +0800

    remove unused bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,9 +17,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
-
             new HomeBundle\HomeBundle(),
-            new ServiceBundle\ServiceBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/ServiceBundle/Tests/Controller/NotesControllerTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ServiceBundle\Tests\Controller;
-
-use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-
-class NotesControllerTest extends WebTestCase
-{
-}
